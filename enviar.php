d3af57b50274a3f27ec79982c2d70c383b3a694d
philippe <phadc@hotmail.com>
2017-02-03 23:31:31 -0200

    sem php

--- enviar.php
+++ enviar.php
@@ -6,24 +6,6 @@
 </head>
 
 <body>
-<?
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$assunto = $_POST['assunto'];
-$mensagem = $_POST['mensagem'];
 
-global $email;
-
-mail ("redclow@gmail.com","$assunto",
-"Nome: $nome
-Email: $email
-Assunto: $assunto
-Mensagem: $mensagem",
-"From: $email"
-);
-
-echo "<p align=center>$nome!sua mensagem foi enviada com sucesso</p>";
-echo "<p align=center>Em breve entraremos em contato</p>";
-?>
 </body>
 </html>
